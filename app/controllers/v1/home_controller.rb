902e1d237089fc349ee32e21d16c3d2cb6d3dc8c
William Belchior <williamsbelchior@gmail.com>
2020-03-14 18:51:40 -0300

    add rubocop

--- app/controllers/v1/home_controller.rb
+++ app/controllers/v1/home_controller.rb
@@ -4,6 +4,6 @@ class V1::HomeController < ApplicationController
   end
 
   def healthz
-    render json: { status: 'ROR API V1 Up and Running!'}
+    render json: { status: 'ROR API V1 Up and RunningROR API V1 Up and RunningROR API V1 Up and RunningROR API V1 Up and RunningROR API V1 Up and RunningROR API V1 Up and RunningROR API V1 Up and RunningROR API V1 Up and RunningROR API V1 Up and RunningROR API V1 Up and RunningROR API V1 Up and RunningROR API V1 Up and RunningROR API V1 Up and RunningROR API V1 Up and RunningROR API V1 Up and RunningROR API V1 Up and RunningROR API V1 Up and Running!'}
   end
 end
